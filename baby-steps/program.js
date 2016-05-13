d3faa019e675d0e52c709ac8c37fc085d093119c
Dylan Thomas <kdylanthomas@gmail.com>
2016-05-13 14:00:05 -0500

    completed first three exercises

---
+++ baby-steps/program.js
@@ -0,0 +1,12 @@
+function sumOfArgs () {
+	var args = process.argv;
+	var sum = 0;
+
+	for (var i = 2; i < args.length; i++) {
+		sum += Number(args[i]);
+	}
+
+	console.log(sum);
+}
+
+sumOfArgs();
